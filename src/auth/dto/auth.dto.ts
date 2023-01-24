4e134da61b1c6260388d790fbcb2cadbb36f87fa
Dmitry Letov <67585149+letovDmitry@users.noreply.github.com>
2023-01-24 15:58:34 +0600

    Initial commit

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail } from "class-validator"
+import { IsNotEmpty, IsString } from "class-validator"
+
+export class AuthDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string
+
+    @IsString()
+    @IsNotEmpty()
+    password: string
+}
