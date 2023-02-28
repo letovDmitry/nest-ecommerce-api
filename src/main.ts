eb1870975c17b326c32c1621112cf03f56a824ee
ughhio <67585149+letovDmitry@users.noreply.github.com>
2023-02-28 19:50:59 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }))
+  
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors();
   await app.listen(3000);
+  
 }
 bootstrap();
 
